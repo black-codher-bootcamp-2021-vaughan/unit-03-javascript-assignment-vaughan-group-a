5b2a0f30435b3022f55bc207404abcc4b114099e
a-gakwaya <agakwaya@gmail.com>
2022-01-17 22:48:23 +0000

    Added modal
    Co-Authored-By: janinebknr <87192949+janinebknr@users.noreply.github.com>
    Co-Authored-By: Riah Lake <iriahbee@users.noreply.github.com>

--- js/index.js
+++ js/index.js
@@ -1,54 +1,101 @@
 import { dates } from "./data.js";
 
+export default function createTimelineList(dates) {
+  const timeline = document.querySelector(".timeline");
 
-export default function createTimelineList(dates){
+  /* Title for Page */
+  const title = document.createElement("h1");
+  const pageTitleText = document.createTextNode("History of Baking Timeline");
+  const wrapper = document.createElement("section");
 
-    const timeline = document.querySelector(".timeline");
+  // The Loop
+  dates.map((dates) => {
+    const timelineItemWrapper = document.createElement("div");
+    timelineItemWrapper.className = "timeline-item";
 
-    /* Title for Page */
-    const title = document.createElement("h1");
-    const pageTitleText = document.createTextNode("History of Baking Timeline");
-    const wrapper = document.createElement("section");
-    
+    // Title
+    const timelineItemTitle = document.createElement("h2");
+    timelineItemTitle.className = "timeline-item-title";
+    timelineItemTitle.textContent = dates.title;
+    timelineItemWrapper.appendChild(timelineItemTitle);
 
-    // The Loop
-    dates.map((dates) => {
+    // Date
+    const timelineItemDate = document.createElement("span");
+    timelineItemDate.className = "timeline-item-date";
+    timelineItemDate.textContent = dates.date;
+    timelineItemWrapper.appendChild(timelineItemDate);
 
-        const timelineItemWrapper = document.createElement("div");
-        timelineItemWrapper.className = "timeline-item"
+    // Summary
+    const timelineItemSummary = document.createElement("p");
+    timelineItemSummary.className = "timeline-item-summary";
+    timelineItemSummary.textContent = dates.summary;
+    timelineItemWrapper.appendChild(timelineItemSummary);
 
-        // Title
-        const timelineItemTitle = document.createElement("h2");
-        timelineItemTitle.className = "timeline-item-title"
-        timelineItemTitle.textContent = dates.title;
-        timelineItemWrapper.appendChild(timelineItemTitle);
+    // Button
+    const timelineItemBT = document.createElement("button");
+    timelineItemBT.className = "timeline-item-more-info";
+    timelineItemBT.textContent = "Read More";
+    timelineItemWrapper.appendChild(timelineItemBT);
+    timelineItemBT.addEventListener("click", openModal);
 
-        // Date
-        const timelineItemDate = document.createElement("span");
-        timelineItemDate.className = "timeline-item-date"
-        timelineItemDate.textContent = dates.date;
-        timelineItemWrapper.appendChild(timelineItemDate);
+    wrapper.appendChild(timelineItemWrapper);
+    const modalWrapper = document.createElement("div");
 
-        // Summary
-        const timelineItemSummary = document.createElement("p");
-        timelineItemSummary.className = "timeline-item-summary"
-        timelineItemSummary.textContent = dates.summary;
-        timelineItemWrapper.appendChild(timelineItemSummary);
+    function openModal() {
+      /* Modal Wrapper for background (div with ID #modal-wrapper) */
+      modalWrapper.setAttribute("id", "modal-wrapper");
 
-        // Button
-        const timelineItemBT = document.createElement("button");
-        timelineItemBT.className = "timeline-item-more-info"
-        timelineItemBT.textContent = "Read More";
-        timelineItemWrapper.appendChild(timelineItemBT);
+      /* Modal Container to hold modal content (div with ID #modal-container) */
+      const modalContainer = document.createElement("div");
+      modalContainer.setAttribute("id", "modal-container");
 
-        wrapper.appendChild(timelineItemWrapper);
-        
-        });
+      /* Close Button (button with ID #modal-close-button) */
+      const modalCloseButton = document.createElement("button");
+      modalCloseButton.setAttribute("id", "modal-close-button");
+      modalCloseButton.setAttribute("type", "reset");
+      modalCloseButton.textContent = "Close";
+      modalContainer.appendChild(modalCloseButton);
+      modalCloseButton.addEventListener("click", closeModal);
 
+      /* Date (p with ID #modal-date) */
+      const modalDate = document.createElement("p");
+      modalDate.setAttribute("id", "modal-date");
+      modalDate.textContent = dates.date;
+      modalContainer.appendChild(modalDate);
 
-title.appendChild(pageTitleText);
-timeline.appendChild(title);
-timeline.appendChild(wrapper);
+      /* Title (h2 with ID #modal-title) */
+      const modalTitle = document.createElement("h2");
+      modalTitle.setAttribute("id", "modal-title");
+      modalTitle.textContent = dates.title;
+      modalContainer.appendChild(modalTitle);
+
+      /* Image (img with ID #modal-image) */
+      const modalImage = document.createElement("img");
+      modalImage.setAttribute("src", dates.image);
+      modalImage.setAttribute("alt", dates.title);
+      modalImage.setAttribute("id", "modal-image");
+      modalContainer.appendChild(modalImage);
+
+      /* Full Description (p with ID #modal-full-description) */
+      const modalFullDescription = document.createElement("p");
+      modalFullDescription.setAttribute("id", "modal-full-description");
+      modalFullDescription.textContent = dates.fullDescription;
+      modalContainer.appendChild(modalFullDescription);
+
+      /* Insert #modal-container to #modal-wrapper */
+      modalWrapper.appendChild(modalContainer);
+
+      /* Insert #modal-wrapper to the body of the HTML */
+      document.body.appendChild(modalWrapper);
+    }
+    function closeModal() {
+      document.body.removeChild(modalWrapper);
+    }
+  });
+
+  title.appendChild(pageTitleText);
+  timeline.appendChild(title);
+  timeline.appendChild(wrapper);
 }
 
 // Call the function
